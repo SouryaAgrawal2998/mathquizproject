8acffb9d9bc6c669d0a01845c099253a0c3dbf63
SouryaAgrawal2998 <81215092+SouryaAgrawal2998@users.noreply.github.com>
2021-08-20 22:12:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+username1= "";
+function addUser(){
+    document.getElementById("player1_name_input").value;
+    localStorage.setItem("username1", username1);
+    document.getElementById("player2_name_input").value;
+    localStorage.setItem("username2", username2);
+
+}
