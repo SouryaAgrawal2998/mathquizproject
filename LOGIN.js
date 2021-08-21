7851117db020dae18c0a3d05d29d9163ae947103
SouryaAgrawal2998 <81215092+SouryaAgrawal2998@users.noreply.github.com>
2021-08-21 23:07:56 +0530

    Add files via upload

---
+++ LOGIN.js
@@ -0,0 +1,9 @@
+username1= "";
+username2="";
+function addUser(){
+    document.getElementById("player1_name_input").value;
+    localStorage.setItem("username1", username1);
+    document.getElementById("player2_name_input").value;
+    localStorage.setItem("username2", username2);
+    
+}
