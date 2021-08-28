fd3436d26d9a9463002e46d84fc4da4725bebc77
SouryaAgrawal2998 <81215092+SouryaAgrawal2998@users.noreply.github.com>
2021-08-28 20:25:49 +0530

    Add files via upload

--- LOGIN.js
+++ LOGIN.js
@@ -1,9 +1,9 @@
 username1= "";
 username2="";
 function addUser(){
-    document.getElementById("player1_name_input").value;
+    username1=document.getElementById("player1_name_input").value;
     localStorage.setItem("username1", username1);
-    document.getElementById("player2_name_input").value;
+    username2=document.getElementById("player2_name_input").value;
     localStorage.setItem("username2", username2);
     window.location= "quiz-game-page.html";
 }
